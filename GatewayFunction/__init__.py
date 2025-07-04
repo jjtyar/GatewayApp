c5c0e970cfb5ccb0d16931c07424072a6f5f0982
jjtyar <jjtyar@gmail.com>
2025-07-04 11:32:04 +0330

    Update __init__.py

--- GatewayFunction/__init__.py
+++ GatewayFunction/__init__.py
@@ -5,8 +5,19 @@ import json
 def main(req: azuref.HttpRequest) -> azuref.HttpResponse:
     logging.info('🚪 Gateway function triggered.')
 
+    req_body = req.get_json()
+        features = [
+            req_body.get('sensor_temp', 0),
+            req_body.get('sensor_gas', 0)
+        ]    
+
+        logging.info('sensor_temp is=')
+        logging.info(req_body.get('sensor_temp', 0))
+
+
     return azuref.HttpResponse(
         json.dumps({"status": "Function is f reachable"}),
         mimetype="application/json",
         status_code=200
     )
+
