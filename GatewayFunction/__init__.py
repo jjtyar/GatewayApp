fdd16d1a1545793da80e35c95fd6287be967bb2a
jjtyar <jjtyar@gmail.com>
2025-07-04 11:59:10 +0330

    Update __init__.py

--- GatewayFunction/__init__.py
+++ GatewayFunction/__init__.py
@@ -3,26 +3,24 @@ import azure.functions as azuref
 import json
 
 def main(req: azuref.HttpRequest) -> azuref.HttpResponse:
-    logging.info('🚪 Gateway function  f triggered.')
+    logging.info('🚪 Gateway function ff triggered.')
 
     try:
-    req_body = req.get_json()
+        req_body = req.get_json()
+
         features = [
             req_body.get('sensor_temp', 0),
             req_body.get('sensor_gas', 0)
         ]    
 
- logging.info('sensor_temp is = {}'.format(req_body.get('sensor_temp', 0)))
+        logging.info('sensor_temp is = {}'.format(req_body.get('sensor_temp', 0)))
 
-
-    return azuref.HttpResponse(
-        json.dumps({"status": "Function is f reachable", 'sensor_temp is = {}'.format(req_body.get('sensor_temp', 0))}),
-        mimetype="application/json",
-        status_code=200
-    )
+        return azuref.HttpResponse(
+            json.dumps({"status": "Function is reachable"}),
+            mimetype="application/json",
+            status_code=200
+        )
 
     except Exception as e:
         logging.error(f"Error: {e}")
-        return azuref.HttpResponse("Error in processing request", status_code
-
-                                   
+        return azuref.HttpResponse("Error in processing request", status_code=500)
