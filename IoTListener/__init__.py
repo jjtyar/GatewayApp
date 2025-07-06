26c7bb900e5e2a8b04753a7dcf1d4ac75e76ee80
jjtyar <jjtyar@gmail.com>
2025-07-06 18:19:20 +0330

    Update __init__.py

--- IoTListener/__init__.py
+++ IoTListener/__init__.py
@@ -5,4 +5,4 @@ def main(event: func.EventHubEvent):
     for e in event:
         message = e.get_body().decode('utf-8')
         logging.info(f"📥 New IoT Hub message: {message}")
-        # TODO: Optionally save to Blob or broadcast to Control Room
+        # TODO: Optionally save to Blob or broadcast to Control Room 
